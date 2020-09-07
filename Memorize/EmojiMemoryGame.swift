30b8a681383e835f6207ce75a8af3ea6be7a7180
Fulton Garcia <fultongarcia@gmail.com>
2020-09-07 15:15:34 -0400

    Code is up to the end of lecture two before doing the homework changes. Implemented MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Fulton Garcia on 9/7/20.
+//  Copyright © 2020 Fulton Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "👽", "💀", "🎃"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count, cardContentFactory: {pairIndex in return emojis[pairIndex]})
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
